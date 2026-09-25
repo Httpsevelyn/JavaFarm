b20fa8c366096f982fd1d43d9b2199da4bff7739
EVELYN BARBOSA DA SILVA <evelyn.bsilva12@senacsp.edu.br>
2026-09-25 20:35:08 -0300

    att celeiro

--- javafarm-main/src/main/java/br/com/joaocarloslima/Celeiro.java
+++ javafarm-main/src/main/java/br/com/joaocarloslima/Celeiro.java
@@ -1,4 +1,89 @@
 package br.com.joaocarloslima;
 
 public class Celeiro {
+    private int capacidade;
+    private int qtdeBatatas;
+    private int qtdeCenouras;
+    private int qtdeMorangos;
+
+    public Celeiro() {
+        capacidade = 20;
+        qtdeBatatas = 5;
+        qtdeCenouras = 3;
+        qtdeMorangos = 1;
+    }
+
+    public void armazenarBatata() {
+        verificarEspacoParaArmazenar();
+        qtdeBatatas += 2;
+    }
+
+    public void armazenarCenoura() {
+        verificarEspacoParaArmazenar();
+        qtdeCenouras += 2;
+    }
+
+    public void armazenarMorango() {
+        verificarEspacoParaArmazenar();
+        qtdeMorangos += 2;
+    }
+
+    public void consumirBatata() {
+        if (qtdeBatatas <= 0) {
+            throw new RuntimeException("Não há batatas no celeiro.");
+        }
+        qtdeBatatas--;
+    }
+
+    public void consumirCenoura() {
+        if (qtdeCenouras <= 0) {
+            throw new RuntimeException("Não há cenouras no celeiro.");
+        }
+        qtdeCenouras--;
+    }
+
+    public void consumirMorango() {
+        if (qtdeMorangos <= 0) {
+            throw new RuntimeException("Não há morangos no celeiro.");
+        }
+        qtdeMorangos--;
+    }
+
+    public int getEspacoDisponivel() {
+        return capacidade - getTotalArmazenado();
+    }
+
+    public double getOcupacao() {
+        return (double) getTotalArmazenado() / capacidade;
+    }
+
+    public boolean celeiroCheio() {
+        return getTotalArmazenado() >= capacidade;
+    }
+
+    private int getTotalArmazenado() {
+        return qtdeBatatas + qtdeCenouras + qtdeMorangos;
+    }
+
+    private void verificarEspacoParaArmazenar() {
+        if (getEspacoDisponivel() < 2) {
+            throw new RuntimeException("O celeiro não possui espaço para armazenar o produto.");
+        }
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public int getQtdeBatatas() {
+        return qtdeBatatas;
+    }
+
+    public int getQtdeCenouras() {
+        return qtdeCenouras;
+    }
+
+    public int getQtdeMorangos() {
+        return qtdeMorangos;
+    }
 }
